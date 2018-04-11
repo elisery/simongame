f4acab02ffbacf9941f9a7638616784bf503d296
Elise Yohm <elisery@hotmail.com>
2018-04-10 23:17:36 -0700

    Work on design elements

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
-  
+  /*
+  1. "Off" switch renders no action from buttons
+    a) pressing off resets game
+  2. "On" means buttons are click-able
+    a) "--" appears in display upon "on" click
+  3. "Start" starts the game from counts of 1
+    a) all values are reset
+  4. "Strict" lights up "circle" - changes color to bright red
+    a) if mistake made, display shows "!!"
+    b) game resets to 1
+  5. Game play:
+    a) game chooses and plays random colour
+    b) user has to repeat colour
+    c) if correct, game chooses another random color and plays the entire array
+    d) if incorrect, game shows "!!" and plays the array until user correct
+  */
 
 });
