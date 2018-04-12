b075579e90c0d2e007a385850d856a6b8ccec3bc
Elise Yohm <elisery@hotmail.com>
2018-04-11 23:21:37 -0700

    Add sound files and set variables

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
 $(document).ready(function() {
   /*
-  
-  3. "Start" starts the game from counts of 1
-    a) all values are reset
+
   4. "Strict" lights up "circle" - changes color to bright red
     a) if mistake made, display shows "!!"
     b) game resets to 1
@@ -12,9 +10,21 @@ $(document).ready(function() {
     c) if correct, game chooses another random color and plays the entire array
     d) if incorrect, game shows "!!" and plays the array until user correct
   */
+  //Set Sound constants
+  const soundGreen = new Audio('./resources/sounds/simonSound1.mp3');
+  const soundRed = new Audio('./resources/sounds/simonSound2.mp3');
+  const soundYellow = new Audio('./resources/sounds/simonSound3.mp3');
+  const soundBlue = new Audio('./resources/sounds/simonSound4.mp3');
+  //Set game quadrant constants
+  const green = {"colour": "#4dff4d", "sound": soundGreen};
+  const red = {"colour": "FF0000", "sound": soundRed};
+  const yellow = {"colour": "#ffff4d", "sound": soundYellow};
+  const blue = {"colour": "#2626FF", "sound": soundBlue};
 
   let strictStatus = false;
+  let gameArr = [];
 
+  //Check if game is on or off
   function checkGameStatus() {
     if ($('input').is(':checked')) {
       return true;
@@ -28,6 +38,7 @@ $(document).ready(function() {
     if (checkGameStatus()) {
       //set display
       $('.counter-display').text('--')
+      green["sound"].play();
     } else {
       //remove display
       $('.counter-display').text('');
@@ -38,7 +49,14 @@ $(document).ready(function() {
   $('.start-button').click(function() {
     //check if game is on
     if (checkGameStatus()) {
-      //do game stuff
+      //reset variables & display
+      gameArr = [];
+      $('.counter-display').text('01');
+      //call game in regular mode or strict mode
+      /*
+      1. call random quad generator + push to array
+      2. iterate array for player
+      */
     } else {
       //render button disabled
       $('.start-button').disabled = true;
@@ -54,13 +72,13 @@ $(document).ready(function() {
         strictStatus = true;
         //turn ON 'light'
         $('#circle').css('background-color', 'red');
-        //set mode
+        //call game in strict mode
 
       } else {
         strictStatus = false;
         //turn OFF 'light'
         $('#circle').css('background-color', '#333333');
-        //set mode
+        //call game in regular mode
 
       }
 
