980f946d39419feb5ca5066fe73b915170d9cacf
Elise Yohm <elisery@hotmail.com>
2018-04-11 22:25:50 -0700

    Add basic button press functionality

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
   /*
-  1. "Off" switch renders no action from buttons
-    a) pressing off resets game
-  2. "On" means buttons are click-able
-    a) "--" appears in display upon "on" click
+  
   3. "Start" starts the game from counts of 1
     a) all values are reset
   4. "Strict" lights up "circle" - changes color to bright red
@@ -15,5 +12,63 @@ $(document).ready(function() {
     c) if correct, game chooses another random color and plays the entire array
     d) if incorrect, game shows "!!" and plays the array until user correct
   */
+
+  let strictStatus = false;
+
+  function checkGameStatus() {
+    if ($('input').is(':checked')) {
+      return true;
+    } else  {
+      return false;
+    }
+  }
+
+  //Click on and off
+  $('.switch').click(function() {
+    if (checkGameStatus()) {
+      //set display
+      $('.counter-display').text('--')
+    } else {
+      //remove display
+      $('.counter-display').text('');
+    }
+  });
+
+  //Click Start Button
+  $('.start-button').click(function() {
+    //check if game is on
+    if (checkGameStatus()) {
+      //do game stuff
+    } else {
+      //render button disabled
+      $('.start-button').disabled = true;
+    }
+  }); //start-button
+
+  //Click Strict Button
+  $('.strict-button').click(function() {
+    //check if game is on
+    if (checkGameStatus()) {
+      //toggle status
+      if (strictStatus === false ) {
+        strictStatus = true;
+        //turn ON 'light'
+        $('#circle').css('background-color', 'red');
+        //set mode
+
+      } else {
+        strictStatus = false;
+        //turn OFF 'light'
+        $('#circle').css('background-color', '#333333');
+        //set mode
+
+      }
+
+    } else {
+      //render button disabled
+      $('.start-button').disabled = true;
+    }
+  }); //strict button
+
 
 });
